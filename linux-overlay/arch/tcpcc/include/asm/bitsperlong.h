a2fe538652821144e772f99f1de8ef7aaec489b3
MiaoMing Chen <miaomingc@stumail.nwu.edu.cn>
2026-08-23 11:06:43 +0800

    m2: delegate word-size constants to asm-generic

--- linux-overlay/arch/tcpcc/include/asm/bitsperlong.h
+++ linux-overlay/arch/tcpcc/include/asm/bitsperlong.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef _ASM_TCPCC_BITSPERLONG_H
 #define _ASM_TCPCC_BITSPERLONG_H
-#include <uapi/asm/bitsperlong.h>
-#define BITS_PER_LONG __BITS_PER_LONG
-#define BITS_PER_LONG_LONG 64
+
+#include <asm-generic/bitsperlong.h>
+
 #endif
