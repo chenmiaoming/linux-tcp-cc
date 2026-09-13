8b0b6c88243bfaa029322a102dc554a97856fbe3
MiaoMing Chen <miaomingc@stumail.nwu.edu.cn>
2026-09-13 22:41:03 +0800

    Memory: reclaim loopback selftest buffers after boot (#124)
    * Memory: reclaim loopback selftest buffers after boot
    
    * CI: cap permanent hosted kernel BSS
    
    * Docs: record config audit and selftest memory strategy

--- linux-overlay/arch/tcpcc/kernel/net.c
+++ linux-overlay/arch/tcpcc/kernel/net.c
@@ -2,6 +2,7 @@
 #include <linux/completion.h>
 #include <linux/err.h>
 #include <linux/errno.h>
+#include <linux/gfp.h>
 #include <linux/in.h>
 #include <linux/init.h>
 #include <linux/kthread.h>
@@ -20,23 +21,62 @@
 #define TCPCC_LOOPBACK_PAYLOAD_SIZE (64U * 1024U)
 #define TCPCC_LOOPBACK_BACKLOG      8
 
-static u8 tcpcc_loopback_tx[TCPCC_LOOPBACK_PAYLOAD_SIZE];
-static u8 tcpcc_loopback_server_rx[TCPCC_LOOPBACK_PAYLOAD_SIZE];
-static u8 tcpcc_loopback_client_rx[TCPCC_LOOPBACK_PAYLOAD_SIZE];
+struct tcpcc_loopback_buffers {
+	u8 *tx;
+	u8 *server_rx;
+	u8 *client_rx;
+};
 
 struct tcpcc_loopback_client_ctx {
 	struct completion done;
+	struct tcpcc_loopback_buffers *buffers;
 	__be16 port;
 	unsigned int round;
 	int result;
 };
 
-static void tcpcc_loopback_fill_payload(unsigned int round)
+static u8 *tcpcc_loopback_alloc_buffer(void)
+{
+	return (u8 *)__get_free_pages(GFP_KERNEL,
+				      get_order(TCPCC_LOOPBACK_PAYLOAD_SIZE));
+}
+
+static void tcpcc_loopback_free_buffer(u8 *buffer)
+{
+	if (buffer)
+		free_pages((unsigned long)buffer,
+			   get_order(TCPCC_LOOPBACK_PAYLOAD_SIZE));
+}
+
+static int tcpcc_loopback_alloc_buffers(struct tcpcc_loopback_buffers *buffers)
+{
+	buffers->tx = tcpcc_loopback_alloc_buffer();
+	buffers->server_rx = tcpcc_loopback_alloc_buffer();
+	buffers->client_rx = tcpcc_loopback_alloc_buffer();
+	if (buffers->tx && buffers->server_rx && buffers->client_rx)
+		return 0;
+
+	tcpcc_loopback_free_buffer(buffers->client_rx);
+	tcpcc_loopback_free_buffer(buffers->server_rx);
+	tcpcc_loopback_free_buffer(buffers->tx);
+	memset(buffers, 0, sizeof(*buffers));
+	return -ENOMEM;
+}
+
+static void tcpcc_loopback_free_buffers(struct tcpcc_loopback_buffers *buffers)
+{
+	tcpcc_loopback_free_buffer(buffers->client_rx);
+	tcpcc_loopback_free_buffer(buffers->server_rx);
+	tcpcc_loopback_free_buffer(buffers->tx);
+	memset(buffers, 0, sizeof(*buffers));
+}
+
+static void tcpcc_loopback_fill_payload(u8 *buffer, unsigned int round)
 {
 	unsigned int i;
 
 	for (i = 0; i < TCPCC_LOOPBACK_PAYLOAD_SIZE; i++)
-		tcpcc_loopback_tx[i] = (u8)((i * 131U + round * 17U) & 0xffU);
+		buffer[i] = (u8)((i * 131U + round * 17U) & 0xffU);
 }
 
 static int tcpcc_loopback_send_all(struct socket *sock, const u8 *buf,
@@ -89,6 +129,7 @@ static int tcpcc_loopback_recv_all(struct socket *sock, u8 *buf, size_t len)
 static int tcpcc_loopback_client(void *arg)
 {
 	struct tcpcc_loopback_client_ctx *ctx = arg;
+	struct tcpcc_loopback_buffers *buffers = ctx->buffers;
 	struct sockaddr_in peer = {
 		.sin_family = AF_INET,
 		.sin_port = ctx->port,
@@ -106,17 +147,17 @@ static int tcpcc_loopback_client(void *arg)
 	if (ret)
 		goto out_release;
 
-	ret = tcpcc_loopback_send_all(sock, tcpcc_loopback_tx,
+	ret = tcpcc_loopback_send_all(sock, buffers->tx,
 				      TCPCC_LOOPBACK_PAYLOAD_SIZE);
 	if (ret)
 		goto out_release;
 
-	ret = tcpcc_loopback_recv_all(sock, tcpcc_loopback_client_rx,
+	ret = tcpcc_loopback_recv_all(sock, buffers->client_rx,
 				      TCPCC_LOOPBACK_PAYLOAD_SIZE);
 	if (ret)
 		goto out_release;
 
-	if (memcmp(tcpcc_loopback_client_rx, tcpcc_loopback_tx,
+	if (memcmp(buffers->client_rx, buffers->tx,
 		   TCPCC_LOOPBACK_PAYLOAD_SIZE)) {
 		ret = -EBADMSG;
 		goto out_release;
@@ -160,7 +201,8 @@ out_unlock:
 }
 
 static int tcpcc_loopback_server_round(struct socket *listener, __be16 port,
-					unsigned int round)
+					unsigned int round,
+					struct tcpcc_loopback_buffers *buffers)
 {
 	struct tcpcc_loopback_client_ctx ctx;
 	struct task_struct *client;
@@ -169,12 +211,13 @@ static int tcpcc_loopback_server_round(struct socket *listener, __be16 port,
 	int ret;
 
 	init_completion(&ctx.done);
+	ctx.buffers = buffers;
 	ctx.port = port;
 	ctx.round = round;
 	ctx.result = -EINPROGRESS;
-	tcpcc_loopback_fill_payload(round);
-	memset(tcpcc_loopback_server_rx, 0, sizeof(tcpcc_loopback_server_rx));
-	memset(tcpcc_loopback_client_rx, 0, sizeof(tcpcc_loopback_client_rx));
+	tcpcc_loopback_fill_payload(buffers->tx, round);
+	memset(buffers->server_rx, 0, TCPCC_LOOPBACK_PAYLOAD_SIZE);
+	memset(buffers->client_rx, 0, TCPCC_LOOPBACK_PAYLOAD_SIZE);
 
 	client = kthread_run(tcpcc_loopback_client, &ctx,
 			     "tcpcc-m4.1/%u", round);
@@ -185,18 +228,18 @@ static int tcpcc_loopback_server_round(struct socket *listener, __be16 port,
 	if (ret)
 		goto out_stop;
 
-	ret = tcpcc_loopback_recv_all(accepted, tcpcc_loopback_server_rx,
+	ret = tcpcc_loopback_recv_all(accepted, buffers->server_rx,
 				      TCPCC_LOOPBACK_PAYLOAD_SIZE);
 	if (ret)
 		goto out_release;
 
-	if (memcmp(tcpcc_loopback_server_rx, tcpcc_loopback_tx,
+	if (memcmp(buffers->server_rx, buffers->tx,
 		   TCPCC_LOOPBACK_PAYLOAD_SIZE)) {
 		ret = -EBADMSG;
 		goto out_release;
 	}
 
-	ret = tcpcc_loopback_send_all(accepted, tcpcc_loopback_server_rx,
+	ret = tcpcc_loopback_send_all(accepted, buffers->server_rx,
 				      TCPCC_LOOPBACK_PAYLOAD_SIZE);
 
 out_release:
@@ -223,6 +266,7 @@ static int __init tcpcc_loopback_tcp_selftest(void)
 		.sin_port = 0,
 		.sin_addr.s_addr = htonl(INADDR_LOOPBACK),
 	};
+	struct tcpcc_loopback_buffers buffers = { };
 	struct socket *listener = NULL;
 	unsigned int round;
 	int ret;
@@ -250,11 +294,16 @@ static int __init tcpcc_loopback_tcp_selftest(void)
 	if (!addr.sin_port)
 		panic("tcpcc: M4.1 listener received zero TCP port");
 
+	ret = tcpcc_loopback_alloc_buffers(&buffers);
+	if (ret)
+		panic("tcpcc: M4.1 temporary buffer allocation failed: %d", ret);
+
 	pr_notice("tcpcc: M4.1 loopback TCP stress starting (%u rounds x %u bytes each direction)\n",
 		  TCPCC_LOOPBACK_TEST_ROUNDS, TCPCC_LOOPBACK_PAYLOAD_SIZE);
 
 	for (round = 0; round < TCPCC_LOOPBACK_TEST_ROUNDS; round++) {
-		ret = tcpcc_loopback_server_round(listener, addr.sin_port, round);
+		ret = tcpcc_loopback_server_round(listener, addr.sin_port, round,
+						  &buffers);
 		if (ret)
 			panic("tcpcc: M4.1 loopback TCP round %u failed: %d",
 			      round, ret);
@@ -262,9 +311,11 @@ static int __init tcpcc_loopback_tcp_selftest(void)
 
 	kernel_sock_shutdown(listener, SHUT_RDWR);
 	sock_release(listener);
+	tcpcc_loopback_free_buffers(&buffers);
 
-	pr_notice("tcpcc: M4.1 loopback TCP stress passed (%u rounds, %u bytes each direction)\n",
-		  TCPCC_LOOPBACK_TEST_ROUNDS, TCPCC_LOOPBACK_PAYLOAD_SIZE);
+	pr_notice("tcpcc: M4.1 loopback TCP stress passed (%u rounds, %u bytes each direction); %u KiB temporary buffers returned to guest RAM\n",
+		  TCPCC_LOOPBACK_TEST_ROUNDS, TCPCC_LOOPBACK_PAYLOAD_SIZE,
+		  (3U * TCPCC_LOOPBACK_PAYLOAD_SIZE) >> 10);
 	return 0;
 }
 fs_initcall_sync(tcpcc_loopback_tcp_selftest);
